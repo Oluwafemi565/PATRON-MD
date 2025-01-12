d981d2eaf5e947132eb44346ede3830c9c31dfeb
Oluwafemi565 <91491959+Oluwafemi565@users.noreply.github.com>
2025-01-12 17:47:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "7042442944"
 
-global.bankowner = "FADARE"
+global.bankowner = "OLUWAFEMI GABRIEL ALABI"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
